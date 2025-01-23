78f197e4549574be3d0e24096cdfee3eee76d7df
garevalo031308 <g.arevalo031308@gmail.com>
2025-01-23 16:30:32 -0500

    Added whitenoise

--- NASAInteractive/settings.py
+++ NASAInteractive/settings.py
@@ -42,10 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'NASAMainPage'
+    'NASAMainPage',
+    'whitenoise.runserver_nostatic'
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
